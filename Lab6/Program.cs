4839a520deb53f0ebc3ed52be003620d8e99d5e8
Charles Lambrecht <lambrechtca@gmail.com>
2017-04-14 15:23:49 -0400

    Added pig latin translator

--- Lab6/Program.cs
+++ Lab6/Program.cs
@@ -12,11 +12,12 @@ namespace Lab6
         {
             do
             {
+                //input from user, convert to array of words
+                string[] sentence = ToStringArray(Console.ReadLine().ToLower());
 
-                string[] sentence = ToStringArray(Console.ReadLine());
+                string translation = PLTranslator(sentence);
 
-                char[] words = ;
-                
+                Console.WriteLine(translation.Trim());
 
             } while (Continue());
 
@@ -28,11 +29,51 @@ namespace Lab6
             return words;
         }
 
-        public static char[] ToCharArray(string[] input)
+        public static string PLTranslator(string[] input)
         {
-            foreach (var item in input)
-            {
+            string PigLatin = "";
+            char[] vowels = new char[5] { 'a', 'e', 'i', 'o', 'u' };
 
+            foreach (string item in input)
+            {
+              
+                if (CheckFirstLetter(item))
+                {
+                    string result = item + "way";
+                    PigLatin = PigLatin + " " + result;
+                }
+                else if (item.IndexOfAny(vowels) == -1)
+                {
+                    PigLatin = PigLatin + " " + item;
+                }
+                else
+                {
+                    string result2 = item.Substring(1) + item.Remove(1);
+
+                    while (!CheckFirstLetter(result2))
+                    {
+                        result2 = result2.Substring(1) + result2.Remove(1);
+                    } 
+
+                    PigLatin = PigLatin + " " + result2 + "ay";
+                }
+                
+            }
+
+            return PigLatin;
+        }
+
+        public static bool CheckFirstLetter(string input)
+        {
+            char[] word = input.ToCharArray();
+
+            if (word[0] == 'a' || word[0] == 'e' || word[0] == 'i' || word[0] == 'o' || word[0] == 'u')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
 
