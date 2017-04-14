b6007962b764aed1d448ba57be4244cc874f4333
Charles Lambrecht <lambrechtca@gmail.com>
2017-04-14 13:53:55 -0400

    Initial commit

---
+++ Lab6/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+
+                string[] sentence = ToStringArray(Console.ReadLine());
+
+                char[] words = ;
+                
+
+            } while (Continue());
+
+        }
+
+        public static string[] ToStringArray(string input)
+        {
+            string[] words = input.Split(' ');
+            return words;
+        }
+
+        public static char[] ToCharArray(string[] input)
+        {
+            foreach (var item in input)
+            {
+
+            }
+        }
+
+        public static bool Continue()
+        {
+            Console.WriteLine("Do you want to continue? (y/n)");
+
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "n")
+            {
+                return false;
+            }
+            else if (input.ToLower() == "y")
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+                Continue();
+                return true;
+            }
+
+
+        }
+    }
+}
